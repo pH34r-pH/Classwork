7c04d3fdf2bd63bd5918e65cde5dddc3890a350e
Tyler <harbin.giuntoli@gmail.com>
2018-01-29 20:11:13 -0500

    Creating a feature branch to implement schedulers, plus a minor change to ScheduleFIle

---
+++ Scheduler/FirstComeFirstServe.c
@@ -0,0 +1,12 @@
+/**
+ * @file FirstComeFirstServe.c
+ */
+
+#include "FirstComeFirstServe.h"
+#include "ScheduleFile.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+bool RunProcess (ScheduleData* inputData){
+    return false;
+}
