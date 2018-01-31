ae8bf2c667cb660cb8e10ac4bc633b4370ae3cb3
Tyler <harbin.giuntoli@gmail.com>
2018-01-30 23:58:59 -0500

    Not a whole lot of new stuff

--- Scheduler/FirstComeFirstServe.c
+++ Scheduler/FirstComeFirstServe.c
@@ -16,9 +16,21 @@ bool RunProcess (ScheduleData* inputData){
 	* How-to:
 	* Processes are completed in the order they are received.
 	*/
+	Vector* Queue;
 	for (int clock = 0; clock < inputData->runLength; ++clock) 
 	{
-		
+		// Iterate through processes to check for new arrivals
+		for (int i = 0; i < numProc; ++i) {
+			if (inputData->processes[i]->arrivalTime == clock) {
+				fprintf(out, "Time %d: %s arrived\n", clock, inputData->processes[i]->processName);
+				VectorAdd(inputData->processes[i], Queue);
+			}
+		}
+		// Continue working though whatever is at index 0 of the Queue, unless the Queue has nothing in it 
+		// or we are out of time.
+
+		// Update everything else to increment wait time
+
 	}
 
 	fclose(out);
