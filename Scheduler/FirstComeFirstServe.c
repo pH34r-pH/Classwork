84bb2829efa8f1f66be87f01057b497337f20de2
Tyler Harbin <harbin.giuntoli@gmail.com>
2018-02-10 15:45:28 -0500

    gettin stuff done

--- Scheduler/FirstComeFirstServe.c
+++ Scheduler/FirstComeFirstServe.c
@@ -7,29 +7,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-bool RunProcess (ScheduleData* inputData){
+void RunProcess (ScheduleData* inputData){
 	FILE *out = fopen("processes.out", "w");
-	int numProc = VectorCount(inputData->processes);
+	int numProc = VectorCount(inputData.processes);
 	fprintf(out, "%d processes\nUsing First Come First Serve\n\n", numProc);
 
 	/*
 	* How-to:
 	* Processes are completed in the order they are received.
 	*/
-	Vector* Queue;
-	for (int clock = 0; clock < inputData->runLength; ++clock) 
+
+	InputProcess queue[numProc];
+	int head, tail, clock;
+	head = tail = clock = 0;
+	int running = 0;
+
+	for (; clock < inputData.runLength; ++clock) 
 	{
 		// Iterate through processes to check for new arrivals
 		for (int i = 0; i < numProc; ++i) {
-			if (inputData->processes[i]->arrivalTime == clock) {
-				fprintf(out, "Time %d: %s arrived\n", clock, inputData->processes[i]->processName);
-				VectorAdd(inputData->processes[i], Queue);
+			if (inputData.processes[i]->arrivalTime == clock) {
+				fprintf(out, "Time %d: %s arrived\n", clock, inputData.processes[i].processName);
+				VectorAdd(inputData.processes[i], queue);
 			}
 		}
-		// Continue working though whatever is at index 0 of the Queue, unless the Queue has nothing in it 
-		// or we are out of time.
+		// Check if we have at least one ready process
+		if(queue[head] != NULL){
+			// Check if we have a process running, or select a new process
+			if(running == 0){
+				fprintf(out, "Time %d: %s selected (burst %d)\n", clock, queue[head].processName, queue[head].remainingTime);
+				running = 1;
+			}
 
-		// Update everything else to increment wait time
+			// Update process counters
+			queue[head].remainingTime -= 1;
+			queue[head].turnaroundTime += 1;
+
+			for(int i = head+1; i < tail; ++i){
+				queue[i].turnaroundTime += 1;
+				queue[i].waitingTime += 1;
+			}
+			
+			// Check if head has finished
+			if(queue[head].remainingTime == 0){
+				fprintf(out, "Time %d: %s finished\n", clock, queue[head].processName);
+				running = 0;
+				++head;
+			}
+		}
+		// No ready processes means we're Idle
+		else {
+			fprintf(out, "Time %d: Idle", clock);
+		}				
+
+		// Show results
+		fprintf(out, "Finished at time %d\n\n", clock);
+		for(int i = 0; i < tail; ++i){
+			fprintf(out, "%s wait %d turnaround %d\n", queue[i].processName, queue[i].waitingTime, queue[i].turnaroundTime);
+		}
 
 	}
 
--- Scheduler/FirstComeFirstServe.h
+++ Scheduler/FirstComeFirstServe.h
@@ -2,9 +2,6 @@
  * @file FirstComeFIrstServe.h
  */
 
-#ifndef SCHEDULER_FIRSTCOMEFIRSTSERVE_H
-#define SCHEDULER_FIRSTCOMEFIRSTSERVE_H
+#pragma once
 
-bool RunProcess (ScheduleData* inputData);
-
-#endif //SCHEDULER_FIRSTCOMEFIRSTSERVE_H
+void RunProcess (ScheduleData* inputData);
