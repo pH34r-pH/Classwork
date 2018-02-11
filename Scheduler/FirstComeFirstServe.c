e7100f86fe2dc49f3347fa923c9f187d95f36b52
Tyler Harbin <harbin.giuntoli@gmail.com>
2018-02-11 00:56:33 -0500

    Completed

--- Scheduler/FirstComeFirstServe.c
+++ Scheduler/FirstComeFirstServe.c
@@ -9,7 +9,7 @@
 #include <stdbool.h>
 
 void RunProcess (ScheduleData* inputData){
-	bool debug = true;		
+	bool debug = false;		
 	
 	if(debug) printf("Starting FCFS\n");
 
@@ -39,64 +39,60 @@ void RunProcess (ScheduleData* inputData){
 	if(debug) printf("Beginning processing:\n");
 	for (; clock < inputData->runLength; ++clock) 
 	{
-		if(debug) printf("heartbeat 1");
 		// Iterate through processes to check for new arrivals
 		for (int i = 0; i < numProc; ++i) {
 			InputProcess* process = VectorGet(i, inputData->processes);
 			if (process->arrivalTime == clock) {
 				if(debug) printf("Time %d: %s arrived\n", clock, process->processName);
 				fprintf(out, "Time %d: %s arrived\n", clock, process->processName);
-				if(debug) printf("heartbeat pre-add");
 				VectorAdd(process, queue);
-				if(debug) printf("heartbeat post-add");
 				++tail;
 			}
 		}
-		if(debug) printf("heartbeat 2");
 		// Check if we have at least one ready process
 		if(head != tail){
-			// Check if we have a process running, or select a new process
-			if(!running){
-				InputProcess* newProcess = VectorGet(head, queue);
-				if(debug) printf("Time %d: %s selected (burst %d)\n", clock, newProcess->processName, newProcess->remainingTime);
-				fprintf(out, "Time %d: %s selected (burst %d)\n", clock, newProcess->processName, newProcess->remainingTime);
-				running = true;
-			}
-			if(debug) printf("heartbeat 3");
-			// Update process counters
+
+			// Check if head has finished
 			InputProcess* headProcess = VectorGet(head, queue);
 			headProcess->remainingTime -= 1;
 			headProcess->turnaroundTime += 1;
 
-			for(int i = head+1; i < tail; ++i){
-				InputProcess* thisProcess = VectorGet(i, queue);
-				thisProcess->turnaroundTime += 1;
-				thisProcess->waitingTime += 1;
-			}
-			if(debug) printf("heartbeat 4");
-			// Check if head has finished
 			if(headProcess->remainingTime == 0){
 				if(debug) printf("Time %d: %s finished\n", clock, headProcess->processName);
 				fprintf(out, "Time %d: %s finished\n", clock, headProcess->processName);
 				running = false;
 				++head;
 			}
+
+			// If head has finished or we are idle, and there is still at least one ready process, select a new process
+			if(!running && head != tail){
+				InputProcess* newProcess = VectorGet(head, queue);
+				if(debug) printf("Time %d: %s selected (burst %d)\n", clock, newProcess->processName, newProcess->remainingTime);
+				fprintf(out, "Time %d: %s selected (burst %d)\n", clock, newProcess->processName, newProcess->remainingTime);
+				running = true;
+			}
+
+			// Update everything else
+			for(int i = head+1; i < tail; ++i){
+				InputProcess* thisProcess = VectorGet(i, queue);
+				thisProcess->turnaroundTime += 1;
+				thisProcess->waitingTime += 1;
+			}
+			
 		}
 		// No ready processes means we're Idle
 		else {
-			if(debug) printf("Time %d: Idle", clock);
-			fprintf(out, "Time %d: Idle", clock);
-		}				
-		if(debug) printf("heartbeat 5");
-		// Show results
-		if(debug) printf("Finished at time %d\n\n", clock);
-		fprintf(out, "Finished at time %d\n\n", clock);
-		for(int i = 0; i < tail; ++i){
-			InputProcess* thisProcess = VectorGet(i, queue);
-			if(debug) printf("%s wait %d turnaround %d\n", thisProcess->processName, thisProcess->waitingTime, thisProcess->turnaroundTime);
-			fprintf(out, "%s wait %d turnaround %d\n", thisProcess->processName, thisProcess->waitingTime, thisProcess->turnaroundTime);
-		}
-
+			if(debug) printf("Time %d: Idle\n", clock);
+			fprintf(out, "Time %d: Idle\n", clock);
+		}		
+	}		
+	// Show results
+	if(debug) printf("Finished at time %d\n\n", clock);
+	fprintf(out, "Finished at time %d\n\n", clock);
+	for(int i = 0; i < tail; ++i){
+		InputProcess* thisProcess = VectorGet(i, queue);
+		if(debug) printf("%s wait %d turnaround %d\n", thisProcess->processName, thisProcess->waitingTime, thisProcess->turnaroundTime);
+		fprintf(out, "%s wait %d turnaround %d\n", thisProcess->processName, thisProcess->waitingTime, thisProcess->turnaroundTime);
 	}
 
 	fclose(out);
