cc4f610e374209dd2acd6db6d0a42f5d89a6b82a
Areax <actuallythisisspam@gmail.com>
2018-02-15 23:28:59 -0500

    Makes changes as requested by nko12.  Camelcase listofProcesses and changes IDLE to Idle.

--- Scheduler/SJF.c
+++ Scheduler/SJF.c
@@ -18,14 +18,14 @@ void RunPreemptiveSJFProcess (ScheduleData* inputData){
 	*/
 
 	LinkedList* list;
-	InputProcess ListofProcesses[numProc];
+	InputProcess listofProcesses[numProc];
 	int clock, quantum, currentQuantum;
 	clock = 0;
 	currentQuantum = quantum = inputData->timeQuantum;
 	bool running = false;
 
 	for(int i = 0; i < numProc; i++) {
-		ListofProcesses[i] = (*(InputProcess*)VectorGet(i, inputData->processes));
+		listofProcesses[i] = (*(InputProcess*)VectorGet(i, inputData->processes));
 	}
 
 	LinkedListCreate(sizeof(int), &list);
@@ -34,8 +34,8 @@ void RunPreemptiveSJFProcess (ScheduleData* inputData){
 
 		if(running) {
 			// Check if the current process just finished
-			if (ListofProcesses[*(int *)LinkedListGet(0, list)].remainingTime == 0) {
-				fprintf(out, "Time %d: %s finished\n", clock, ListofProcesses[*(int *)LinkedListGet(0, list)].processName);
+			if (listofProcesses[*(int *)LinkedListGet(0, list)].remainingTime == 0) {
+				fprintf(out, "Time %d: %s finished\n", clock, listofProcesses[*(int *)LinkedListGet(0, list)].processName);
 				LinkedListRemove(0, list);
 				running = false;
 				currentQuantum = quantum;
@@ -44,12 +44,12 @@ void RunPreemptiveSJFProcess (ScheduleData* inputData){
 
 		// Check for new arrivals
 		for (int i = 0; i < numProc; ++i) {
-			if (ListofProcesses[i].arrivalTime == clock) {
+			if (listofProcesses[i].arrivalTime == clock) {
 				int j = 0;
 				// Check to see if the new arrival should be inserted inside the linked list
 				for (; j < LinkedListCount(list); j++)
 				{
-					if(ListofProcesses[i].remainingTime < ListofProcesses[*(int*)LinkedListGet(j, list)].remainingTime) {
+					if(listofProcesses[i].remainingTime < listofProcesses[*(int*)LinkedListGet(j, list)].remainingTime) {
 						LinkedListInsert(&i, j, list);
 						// no longer running the current process because a new one has higher priority
 						if(j == 0) {
@@ -62,7 +62,7 @@ void RunPreemptiveSJFProcess (ScheduleData* inputData){
 				if(j == LinkedListCount(list)) {
 					LinkedListPush(&i, list);
 				}
-				fprintf(out, "Time %d: %s arrived\n", clock, ListofProcesses[i].processName);
+				fprintf(out, "Time %d: %s arrived\n", clock, listofProcesses[i].processName);
 			}
 		}
 
@@ -78,28 +78,28 @@ void RunPreemptiveSJFProcess (ScheduleData* inputData){
 				fprintf(
 					out, "Time %d: %s selected (burst %d)\n",
 					clock,
-					ListofProcesses[*(int *)LinkedListGet(0, list)].processName,
-					ListofProcesses[*(int *)LinkedListGet(0, list)].remainingTime
+					listofProcesses[*(int *)LinkedListGet(0, list)].processName,
+					listofProcesses[*(int *)LinkedListGet(0, list)].remainingTime
 				);
 				running = true;
 			}
 
 			// Update process counters
-			ListofProcesses[*(int *)LinkedListGet(0, list)].remainingTime -= 1;
-			ListofProcesses[*(int *)LinkedListGet(0, list)].turnaroundTime += 1;
+			listofProcesses[*(int *)LinkedListGet(0, list)].remainingTime -= 1;
+			listofProcesses[*(int *)LinkedListGet(0, list)].turnaroundTime += 1;
 			currentQuantum--;
 
 			for (int i = 0; i < LinkedListCount(list); i++) {
-				if(ListofProcesses[*(int *)LinkedListGet(i, list)].remainingTime > 0 &&
+				if(listofProcesses[*(int *)LinkedListGet(i, list)].remainingTime > 0 &&
 					i != 0) {
-						ListofProcesses[*(int *)LinkedListGet(i, list)].waitingTime += 1;
-					ListofProcesses[*(int *)LinkedListGet(i, list)].turnaroundTime += 1;
+						listofProcesses[*(int *)LinkedListGet(i, list)].waitingTime += 1;
+					listofProcesses[*(int *)LinkedListGet(i, list)].turnaroundTime += 1;
 				}
 			}
 		}
 		// If there is no available process and we have clock time left, we are Idle
 		else {
-			fprintf(out, "Time %d: IDLE\n", clock);
+			fprintf(out, "Time %d: Idle\n", clock);
 		}
 	}
 
@@ -109,9 +109,9 @@ void RunPreemptiveSJFProcess (ScheduleData* inputData){
 		fprintf(
 			out,
 			"%s wait %d turnaround %d\n",
-			ListofProcesses[i].processName,
-			ListofProcesses[i].waitingTime,
-			ListofProcesses[i].turnaroundTime
+			listofProcesses[i].processName,
+			listofProcesses[i].waitingTime,
+			listofProcesses[i].turnaroundTime
 		);
 	}
 
